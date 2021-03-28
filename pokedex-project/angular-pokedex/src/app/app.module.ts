a7965bee4eb146fa7668a36c6d100a60ba7dba37
matthewlchristensen <mattswimftw@gmail.com>
2021-03-28 16:25:50 -0700

    Initialized view structure

--- pokedex-project/angular-pokedex/src/app/app.module.ts
+++ pokedex-project/angular-pokedex/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { PokedexPageComponent } from './pokedex-page/pokedex-page.component';
+import { TableViewComponent } from './pokedex-page/table-view/table-view.component';
+import { CardsViewComponent } from './pokedex-page/cards-view/cards-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    PokedexPageComponent,
+    TableViewComponent,
+    CardsViewComponent
   ],
   imports: [
     BrowserModule,
