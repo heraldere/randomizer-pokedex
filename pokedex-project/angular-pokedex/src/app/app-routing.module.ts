b8d1b6b35e345277f263a5791c0fbf711c607548
JenniNelson <jennymarienelson@gmail.com>
2021-03-28 23:47:11 -0700

    Header tabs and pokedex halves

--- pokedex-project/angular-pokedex/src/app/app-routing.module.ts
+++ pokedex-project/angular-pokedex/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PokedexPageComponent } from './pokedex-page/pokedex-page.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  // { path: '', redirectTo:'/home'},
+  { path: 'home', component: HomeComponent },
+  { path: 'pokedex', component: PokedexPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
